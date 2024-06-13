d3c11ed3be7f3ce7a6bc09e9b2bf23779fc21757
daniel gomes <daniel.gomes@valetelecom.local>
2024-06-13 09:42:19 -0300

    controlador de tempo dos eventos

--- src/components/DivEventos.jsx
+++ src/components/DivEventos.jsx
@@ -1,20 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
+import { parse, differenceInDays, differenceInHours, differenceInMinutes } from 'date-fns';
 import './DivEventos.css';
 
-const DivEventos = ({ eventKey, title, text, inicio, fim }) => {
+const calculaDiferencaTempo = (inicio, fim) => {
+  const agora = new Date()
+
+
+  const verificaDias = differenceInDays(inicio, agora)
+  const verificaHoras = differenceInHours(inicio, agora) % 24
+  const verificaMinutos = differenceInMinutes(inicio, agora) % 60
+
+  if(verificaDias > 0 || verificaHoras > 0 || verificaMinutos > 0)
+    return "Em breve"
+
+  const dias = differenceInDays(fim, agora);
+  const horas = differenceInHours(fim, agora) % 24;
+  const minutos = differenceInMinutes(fim, agora) % 60;
+
+  if(dias <= 0 && horas <= 0 && minutos <= 0)
+    return "Evento finalizado"
+  return `${dias}d ${horas}h ${minutos}m`;
+}
+
+const DivEventos = ({ eventKey, title, text, dataInicio, horaInicio, dataFim, horaFim }) => {
+  const inicio = parse(`${dataInicio} ${horaInicio}`, 'dd/MM HH:mm', new Date())
+  const fim = parse(`${dataFim} ${horaFim}`, 'dd/MM HH:mm', new Date())
+  const [diferencaTempo, setDiferencaTempo] = useState(calculaDiferencaTempo(inicio, fim))
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setDiferencaTempo(calculaDiferencaTempo(inicio, fim));
+    }, 60000);
+
+    return () => clearInterval(intervalo);
+  }, [fim]);
 
   const bodyStyle = {
-    background: "#fff",
-    color: "#2C2D33",
+    background: "#3C3C3C",
+    color: "#fff",
     textAlign: "left"
   }
 
   return (
     <Accordion.Item eventKey={eventKey} className='mb-4 position-relative' >
       <Accordion.Header className={"header"}>{title}</Accordion.Header>
-      <span className='data-evento'>{inicio} ~ {fim}</span>
+      <span className='data-evento'>{diferencaTempo}</span>
       <Accordion.Body style={bodyStyle}>
-        Descrição: {text}
+        <p>{dataInicio} ~ {dataFim}</p> Descrição: {text}
       </Accordion.Body>
     </Accordion.Item>
   )
--- src/components/page/Events.jsx
+++ src/components/page/Events.jsx
@@ -1,35 +1,50 @@
-import Accordion from 'react-bootstrap/Accordion';
-import DivEventos from '../DivEventos';
+import Accordion from "react-bootstrap/Accordion";
+import DivEventos from "../DivEventos";
 
 const Events = () => {
   return (
-    <Accordion className='w-75'>
-      <DivEventos 
-        eventKey="0" 
-        title="Overdash Club" 
-        inicio="30/05" 
-        fim="17/06" 
-        text="Texto aqui" 
+    <Accordion className="w-75">
+      <DivEventos
+        eventKey="0"
+        title="Overdash Club"
+        dataInicio="30/05"
+        dataFim="17/06"
+        horaInicio="23:00"
+        horaFim="06:00"
+        text="-"
       />
 
-      <DivEventos 
-        eventKey="1" 
-        title="Intensive Training" 
-        inicio="06/06" 
-        fim="13/06" 
-        text="Texto aqui 2" 
+      <DivEventos
+        eventKey="1"
+        title="Intensive Training"
+        dataInicio="06/06"
+        dataFim="13/06"
+        horaInicio="06:00"
+        horaFim="06:00"
+        text="-"
       />
 
-      <DivEventos 
-        eventKey="2" 
-        title="Alloy Smelt" 
-        inicio="05/06" 
-        fim="27/06" 
-        text="Texto aqui 3" 
+      <DivEventos
+        eventKey="2"
+        title="Alloy Smelt"
+        dataInicio="05/06"
+        dataFim="27/06"
+        horaInicio="20:00"
+        horaFim="06:00"
+        text="-"
       />
 
+      <DivEventos
+        eventKey="3"
+        title="Wuthering Exploration"
+        dataInicio="13/06"
+        dataFim="27/06"
+        horaInicio="12:00"
+        horaFim="05:59"
+        text="Complete Missões de Exploração diariamente para obter Pontos de Exploração e reivindique recompensas após alcançar marcos de Pontos de Exploração."
+      />
     </Accordion>
-  )
-}
+  );
+};
 
 export default Events;
